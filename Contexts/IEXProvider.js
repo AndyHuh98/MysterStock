75fd17ecb7181d802aef61699fbe8a0935a989c0
AndyHuh98 <andrewhe@uark.edu>
2020-12-29 15:47:10 -0600

    started loading screen, will break off into new branch

--- Contexts/IEXProvider.js
+++ Contexts/IEXProvider.js
@@ -76,6 +76,8 @@ export default function IEXProvider({children}) {
     }
   }
 
+  // TODO: Test Live updates of this, also make it refresh every 5 minutes
+  // as well as just each time the window changes (already implemented potentially)
   async function fetchCompanyIntradayData(compSymbol) {
     const companyIntradayURL = `${api_base_url}/intradaydata?symbol=${compSymbol}`;
     console.log('IEXProvider(): ' + companyIntradayURL);
