95c0fe872a83d0c94d15455783e1c5da554e7ed8
AndyHuh98 <andrewhe@uark.edu>
2020-12-29 21:24:56 -0600

    Added icons to tabs

--- Components/Navigation/MainTabNavigator.js
+++ Components/Navigation/MainTabNavigator.js
@@ -1,5 +1,7 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import RandomStockScreen from '../TabScreens/RandomStockScreen';
 import AccountScreen from '../TabScreens/AccountScreen';
 
@@ -7,7 +9,25 @@ const Tab = createBottomTabNavigator();
 
 export default function MainTabNavigator(props) {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Stock') {
+            iconName = focused ? 'cash' : 'cash-outline';
+          } else if (route.name === 'Account') {
+            iconName = focused ? 'person-circle' : 'person-circle-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'green',
+        inactiveTintColor: 'gray',
+      }}>
       <Tab.Screen
         name="Stock"
         children={() => <RandomStockScreen navigation={props.navigation} />}
