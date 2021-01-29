d8126b417562fc2c7e1c0bc9804701167495d714
AndyHuh98 <andrewhe@uark.edu>
2021-01-29 14:10:33 -0600

    removing logs from production build

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+function noop() {}
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log = noop;
+  console.error = noop;
+  console.warn = noop;
+}
